8bb77019079604292d542317be2b62755638b362
Alex <alex@pixelkraft.net>
2018-05-03 09:30:14 -0400

    Prepare for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://akmalu21:Aralogan12358!@ds253879.mlab.com:53879/coinoid"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
